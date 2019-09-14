e450ef2f4c0f3926d039393dd602dbebbeeeed9d
Mehul Khandelwal <18ucs096@lnmiit.ac.in>
2019-09-14 11:58:25 +0530

    hi

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.post('/register',(req,res)=>register.handleRegister(req,res,db,bcrypt))
 app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)})
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
-app.listen(process.env.PORT,()=>{
-	console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3001,()=>{
+	console.log('app is running on port 3000');
 })
 
 
